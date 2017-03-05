81189a965f65733da29b8ae5a5b7a37be2a9dc4a
valkos <svetavv90@i.ua>
2017-03-05 22:37:14 +0100

    adding logic for previous years calculation

--- src/main/java/pl/parser/nbp/Parser/XmlExtractor.java
+++ src/main/java/pl/parser/nbp/Parser/XmlExtractor.java
@@ -16,9 +16,11 @@ import java.util.TreeMap;
  * Created by valkos on 04.03.17.
  */
 public class XmlExtractor {
-    private static final String DIRECTORY_NAMES = "http://www.nbp.pl/kursy/xml/dir.txt";
+    private static final String NAMES = "http://www.nbp.pl/kursy/xml/dir";
+    private static final String EXTENSION = ".txt";
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.BASIC_ISO_DATE;
     private static final String UTF8_BOM = "\uFEFF"; //it is possible to keep the result with BOM prefix only manually
+    private Map<LocalDate, String> files = new TreeMap<>();
 
     private LocalDate dateFrom;
     private LocalDate dateTo;
@@ -29,8 +31,21 @@ public class XmlExtractor {
     }
 
     public List<String> extractXmlNames() {
-        Map<LocalDate, String> files = getAllFiles();
-        List<String> chosenFiles = new ArrayList<>(365);
+        int currentYear = Year.now().getValue();
+        int yearFrom = dateFrom.getYear();
+        int yearTo = dateTo.getYear();
+        String readFromCurrentYear = NAMES + EXTENSION;
+        if (currentYear == yearFrom && currentYear == yearTo) {
+            getAllFiles(readFromCurrentYear);
+        } else {
+            if (currentYear == yearTo) {
+                getAllFiles(readFromCurrentYear);
+                readNamesForPreviuosYears(yearFrom, yearTo-1);
+            } else {
+                readNamesForPreviuosYears(yearFrom, yearTo);
+            }
+        }
+        List<String> chosenFiles = new ArrayList<>();
         files.forEach( (key, value) -> {
             if ( key.isEqual(dateFrom) ||(key.isAfter(dateFrom) && key.isBefore(dateTo)) || key.isEqual(dateTo)) {
                 chosenFiles.add(value);
@@ -39,28 +54,32 @@ public class XmlExtractor {
         return chosenFiles;
     }
 
-    private Map<LocalDate, String> getAllFiles() {
+    private void readNamesForPreviuosYears(int yearFrom, int yearTo) {
+        for (int value = yearFrom; value <= yearTo; value++ ) {
+            getAllFiles(NAMES + value + EXTENSION);
+        }
+    }
+
+    private void getAllFiles(String readFrom) {
         final String modifier = "c";
-        Map<LocalDate, String> files = new TreeMap<>();
         try (BufferedReader in = new BufferedReader(
-                new InputStreamReader(new URL(DIRECTORY_NAMES).openStream()))) {
+                new InputStreamReader(new URL(readFrom).openStream()))) {
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
                 if (inputLine.startsWith(modifier)) {
-                    adaptedFields(files, inputLine);
+                    adaptedFields(inputLine);
                 } else if (inputLine.startsWith(UTF8_BOM + modifier)) {
-                    adaptedFields(files, inputLine.substring(1));
+                    adaptedFields(inputLine.substring(1));
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return files;
     }
 
-    private Map<LocalDate, String> adaptedFields(Map<LocalDate, String> files, String inputLine) {
-        final Year currentYear = Year.now();
-        LocalDate date = LocalDate.parse(currentYear + inputLine.substring(7), FORMATTER); // skipping cxxxzYY symbols
+    private Map<LocalDate, String> adaptedFields(String inputLine) {
+        final String partOfYear = "20";
+        LocalDate date = LocalDate.parse(partOfYear + inputLine.substring(5), FORMATTER); // skipping cxxxzYY symbols
         files.put(date, inputLine);
         return files;
     }
